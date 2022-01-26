742ee451d80c6659c053a1b0883281801ea707ef
Alice Souza <alicepsouzaba1998@gmail.com>
2022-01-25 22:00:31 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
-import '../styles/globals.css'
+import Head from "next/head";
+
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: "Open Sans", sans-serif;
+      }
+      /* App fit Height */
+      html,
+      body,
+      #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */
+    `}</style>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title>Aluracord</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+};
 
-export default MyApp
+export default MyApp;
---
+++ pages/chat.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function chat() {
+  return (
+    <div>
+      <p>Chat aqui</p>
+    </div>
+  )
+};
